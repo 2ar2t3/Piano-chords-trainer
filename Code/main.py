f1e43bad78643a8029560917eac818a35b273fd4
2ar2t3 <arthur.aussedat-pretet.1@ens.etsmtl.ca>
2025-07-02 00:09:52 -0400

    Initial commit, automatically runs the only available exercice yet but will contain the UI when more exercices will be made.

---
+++ Code/main.py
@@ -0,0 +1,12 @@
+"""
+main.py
+-------
+Lance simplement la boucle de quiz définie dans single_note.py
+Contiendra ultérierement l'UI de selection de l'exercice à faire.
+"""
+
+from single_note import *
+
+
+if __name__ == "__main__":
+    quiz_loop()
