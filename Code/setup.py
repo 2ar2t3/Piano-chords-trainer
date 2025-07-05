a44c76622046068ec9c78a0273a6cb80a8a2cdf7
2ar2t3 <arthur.aussedat-pretet.1@ens.etsmtl.ca>
2025-07-05 17:13:47 -0400

    Ajout d'un module pour gérer la connexion avec le piano

--- Code/setup.py
+++ Code/setup.py
@@ -8,15 +8,6 @@ setup.py
 import sys
 import mido
 
-# Notes names in MIDI numerical order
-NOTE_NAMES = ["C", "C#", "D", "D#", "E",
-              "F", "F#", "G", "G#", "A", "A#", "B"]
-
-# Dictionnaires de conversion
-NOTE_TO_PC = {name: pc for pc, name in enumerate(NOTE_NAMES)}
-PC_TO_NOTE = {pc: name for name, pc in NOTE_TO_PC.items()}
-
-
 def choose_input_port() -> mido.ports.BaseInput:
     """Return the first available MIDI-IN port or exit."""
     ports = mido.get_input_names()
@@ -26,3 +17,8 @@ def choose_input_port() -> mido.ports.BaseInput:
 
     # On suppose qu'un seul clavier est branché
     return mido.open_input(ports[0])
+
+def flush_port(port):
+    """Vide immédiatement tout le buffer MIDI (note_on, note_off, CC, etc.)."""
+    for _ in port.iter_pending():
+        pass              # on lit et on jette
