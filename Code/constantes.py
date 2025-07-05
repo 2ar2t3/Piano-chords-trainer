83e2527b1cccecf6b3184c36d028964d24804ac4
2ar2t3 <arthur.aussedat-pretet.1@ens.etsmtl.ca>
2025-07-05 17:12:28 -0400

    Déplacement des constantes contenant les accords et notes dans un module annexe

---
+++ Code/constantes.py
@@ -0,0 +1,39 @@
+# constants.py
+
+import random
+
+NOTE_NAMES = [
+    "C",
+    random.choice(["C#", "Db"]),
+    "D",
+    random.choice(["D#", "Eb"]),
+    "E",
+    "F",
+    random.choice(["F#", "Gb"]),
+    "G",
+    random.choice(["G#", "Ab"]),
+    "A",
+    random.choice(["A#", "Bb"]),
+    "B",
+]
+
+NOTE_TO_PC = {name: pc for pc, name in enumerate(NOTE_NAMES)}
+PC_TO_NOTE = {pc: name for name, pc in NOTE_TO_PC.items()}
+
+# Intervalles (en demi-tons) par type d’accord
+CHORD_INTERVALS = {
+    "maj":  (0, 4, 7),        # root, M3, P5
+    "min":  (0, 3, 7),        # root, m3, P5
+    "7":    (0, 4, 7, 10),    # dominante 7
+    "maj7": (0, 4, 7, 11),    # majeure 7
+    "min7": (0, 3, 7, 10),
+}
+
+# Génération automatique des accords
+CHORDS = {
+    f"{root}{suffix}":
+        # Génére l'accord décallant la fondammentale depuis les intervalles
+        { (NOTE_TO_PC[root] + interval) % 12 for interval in intervals }
+    # Génére le nom de l'accord
+    for root in NOTE_NAMES
+    for suffix, intervals in CHORD_INTERVALS.items()}
